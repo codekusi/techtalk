5cda1004f469753a160cc05bf799700b472b25fc
Markus Noth <markus.noth@swisstopo.ch>
2025-06-04 12:57:36 +0200

    FEAT: full example

--- greetings/hello.py
+++ greetings/hello.py
@@ -1,6 +1,12 @@
 """ I say hello """
 import sys
 
-names = sys.argv[1:] if len(sys.argv) > 1 else ["world"]
 
-print(f"Hello {' and '.join(names)}!")
+def get_greeting(*names: list[str]) -> str:
+    """Return a greeting message."""
+    return f"Hello {' and '.join(names) if names else ''}!"
+
+
+if __name__ == "__main__":
+    _names = sys.argv[1:] if len(sys.argv) > 1 else ["World"]
+    print(get_greeting(*_names))
